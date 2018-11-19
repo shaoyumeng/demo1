f25117095f8e429ef0789a89eeac6aabca7fa955
shaoyumeng <shaoyumeng@gikoosz.com>
2018-11-19 16:55:25 +0800

    first project

--- src/cn/sym/servlet/HelloServlet.java
+++ src/cn/sym/servlet/HelloServlet.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 public class HelloServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         System.out.println();
+        int a = 2;
         response.getWriter().write("for you thousands time");
     }
 
